26ad26eda36443775d624863ff01913f0b982dcd
André Gonçalves <63752183+hyperdregon@users.noreply.github.com>
2022-01-21 21:09:29 +0000

    Bug fixes.

--- swapi.h
+++ swapi.h
@@ -4,11 +4,12 @@
 
 //WINDOW
 void swapi_init(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
-HWND swapi_createwindow(LPCWSTR windowname, int positionx, int positiony, int width, int height);
+void swapi_initwindow(LPCWSTR windowname, int positionx, int positiony, int width, int height);
 void swapi_changestyle(DWORD newstyle);
 void swapi_changecursor(char *name);
 void swapi_changeicon(char *name);
 void swapi_addwindowevent(char *windowevent, void (*func)());
+HWND swapi_createwindow();
 void swapi_showwindow();
 void swapi_destroywindow();
 
